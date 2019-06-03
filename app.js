8de8ff764eb9dfefdd1d8fff5272a9c2d4fdb7a4
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-06-02 23:25:55 -0400

    fill and empty js

---
+++ app.js
@@ -0,0 +1,17 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+//fill listeners 
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+//drag fucntions
+function dragStart(){
+    this.className += ' hold';
+    setTimeout(()=> (this.className = 'invisible'), 0)
+}
+
+function dragEnd(){
+    this.classNAme = 'fill';
+}
